a3f09182704488943769914dfbef61efdf4bd44d
sai kg <gskrishna44@gmail.com>
2015-12-07 14:48:10 -0500

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,10 +4,5 @@ class Address < ActiveRecord::Base
   validates :street, presence: true
   validates :city, presence: true
   validates :state, presence: true
-
-  # Notice: The requirement says 'at most 5 digits, so the regexp match any thing from 1 digit to 5 digits
   validates :zip, presence:true, format: { with: /(^\d{,5}$)/, message: 'must be at most 5 digits'}
-
-  # Ordinary U.S. zip validation /^[\d]{5}(-[\d]{4})?$/
-
 end
